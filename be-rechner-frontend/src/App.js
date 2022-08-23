61c4d476e72019183b1efedec70ad14a9a39d4b7
romanomantek <92530264+romanomantek@users.noreply.github.com>
2022-08-23 11:06:39 +0200

    routes und so

--- be-rechner-frontend/src/App.js
+++ be-rechner-frontend/src/App.js
@@ -1,27 +1,21 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from './components/Footer';
+import "./App.css";
 
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App"></div>
+          <div className="App-header"></div>
+            <div className="App-logo"></div>
+              <div className="Footer">
+                <Route path="./components/Footer.js" />
+              </div>
+      </Router>
+    );
+  }
+}      
 export default App;
