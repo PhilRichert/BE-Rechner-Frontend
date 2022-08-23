cb4b7ad8238386c361a57c0d915f5176eb9e1493
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-08-23 14:31:52 +0200

    Update Footer.js

--- be-rechner-frontend/src/components/Footer.js
+++ be-rechner-frontend/src/components/Footer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+// import "./Footer.css";
 
-function Footer() {
-  return (
-    <div>
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="container-fluid bg-dark text-white mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+
+              
+                <div>
      <p>&copy; by Philip R., René G., Romano M.</p>
     </div>
-  );
+            </div>
+            <div className="col-md-6">
+              <div>
+           
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
