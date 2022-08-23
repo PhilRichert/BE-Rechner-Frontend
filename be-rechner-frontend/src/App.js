e5e3d2e4c04105e3ede50aafb102bfc176021d74
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-08-23 12:32:07 +0200

    Update App.js

--- be-rechner-frontend/src/App.js
+++ be-rechner-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from './components/Navbar';
 import Footer from './components/Footer';
 import "./App.css";
 
