6d08441cc649d7c2fe3f8da350aad1212b0537a9
romanomantek <92530264+romanomantek@users.noreply.github.com>
2022-08-23 10:54:11 +0200

    Footer erstellt

--- be-rechner-frontend/src/App.js
+++ be-rechner-frontend/src/App.js
@@ -1,3 +1,5 @@
+import Footer from './Footer';
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ be-rechner-frontend/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+     <p>&copy; by Philip R., René G., Romano M.</p>
+    </div>
+  );
+}
+
+export default Footer;
