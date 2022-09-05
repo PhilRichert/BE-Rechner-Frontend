d11ca29f4d6dd4ec9dd22ae925caace83ce14023
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-09-05 09:34:39 +0200

    NewEntry, Einstellungen, Main.css

--- be-rechner-frontend/src/pages/Einstellungen.js
+++ be-rechner-frontend/src/pages/Einstellungen.js
@@ -6,6 +6,7 @@ import Footer from "../components/Footer.js";
 import Navbar from "../components/Navbar.js";
 import Banner from "../components/Banner.js";
 import Prefooter from "../components/Prefooter.js";
+import lolli from "../pages/images/diabetes-2.jpg";
 
 export default function Einstellungen() {
   const [title, setTitle] = useState(0);
@@ -21,14 +22,14 @@ export default function Einstellungen() {
       method: "POST",
       headers: {
         Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
+        "Content-Type": "application/json;charset=UTF-8"
       },
       data: {
         Faktor_morgens: title,
         Faktor_mittags: title2,
         Faktor_abends: title3,
-        Faktor_nachts: title4,
-      },
+        Faktor_nachts: title4
+      }
     };
     axios(options).then((response) => {
       console.log(response.status);
@@ -38,82 +39,87 @@ export default function Einstellungen() {
   return (
     <div>
       <Navbar />
-      <Banner title="Welcome Sugies!" />
-      <div className="card">
-        <div className="card-body">
-          <h2>Einstellungen</h2>
-          <form className="form-control form-control-sm">
-            <label>Faktor morgens</label>
-            <input
-              type="text"
-              placeholder="0"
-              defaultValue={title}
-              onChange={(e) => {
-                if (!title) {
-                  return null;
-                } else if (title !== e.target.value) {
-                  setTitle(e.target.value);
-                }
-              }}
-            />
-          </form>
-          <form className="form-control form-control-sm">
-            <label>Faktor Mittags</label>
-            <input
-              type="text"
-              value={title2}
-              placeholder="0"
-              onChange={(e) => {
-                if (!title2) {
-                  return null;
-                } else if (title2 !== e.target.value) {
-                  setTitle2(e.target.value);
-                }
-              }}
-            />
-          </form>
-          <form className="form-control form-control-sm">
-            <label>Faktor Abends</label>
-            <input
-              type="text"
-              value={title3}
-              placeholder="0"
-              onChange={(e) => {
-                if (!title3) {
-                  return null;
-                } else if (title3 !== e.target.value) {
-                  setTitle3(e.target.value);
-                }
-              }}
-            />
-          </form>
-          <form class="form-control form-control-sm">
-            <label>Faktor Nachts</label>
-            <input
-              type="text"
-              value={title4}
-              placeholder="0"
-              onChange={(e) => {
-                if (!title4) {
-                  return null;
-                } else if (title4 !== e.target.value) {
-                  setTitle4(e.target.value);
-                }
-              }}
-            />
-          </form>
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="submit"
-            onClick={() => handleclick()}
-          >
-            Bestätigen
-          </button>
+      <Banner title="Einstellungen" />
+      <div className="container text-left main-content">
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <img src={lolli} alt="Diabeteswerte" width="420" height="320" />
+          </div>
+          <div className="col-md-6">
+            <p>Bitte gib hier deine aktuellen Werte ein.</p>
+            <div className="einstellungen">
+              <form class="form-wert">
+                <label>Faktor morgens:</label>
+                <input
+                  type="text"
+                  placeholder="0"
+                  defaultValue={title}
+                  onChange={(e) => {
+                    if (!title) {
+                      return null;
+                    } else if (title !== e.target.value) {
+                      setTitle(e.target.value);
+                    }
+                  }}
+                />
+              </form>
+              <form class="form-wert">
+                <label>Faktor Mittags:</label>
+                <input
+                  type="text"
+                  value={title2}
+                  placeholder="0"
+                  onChange={(e) => {
+                    if (!title2) {
+                      return null;
+                    } else if (title2 !== e.target.value) {
+                      setTitle2(e.target.value);
+                    }
+                  }}
+                />
+              </form>
+              <form class="form-wert">
+                <label>Faktor Abends: </label>
+                <input
+                  type="text"
+                  value={title3}
+                  placeholder="0"
+                  onChange={(e) => {
+                    if (!title3) {
+                      return null;
+                    } else if (title3 !== e.target.value) {
+                      setTitle3(e.target.value);
+                    }
+                  }}
+                />
+              </form>
+              <form class="form-wert">
+                <label>Faktor Nachts: </label>
+                <input
+                  type="text"
+                  value={title4}
+                  placeholder="0"
+                  onChange={(e) => {
+                    if (!title4) {
+                      return null;
+                    } else if (title4 !== e.target.value) {
+                      setTitle4(e.target.value);
+                    }
+                  }}
+                />
+              </form>
+
+              <button
+                className="btn btn-primary"
+                type="submit"
+                onClick={() => handleclick()}
+              >
+                Bestätigen
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
       <Prefooter />
       <Footer />
     </div>
--- be-rechner-frontend/src/pages/NewEntry.js
+++ be-rechner-frontend/src/pages/NewEntry.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Accordion from "react-bootstrap/Accordion";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Prefooter from "../components/Prefooter";
 import Footer from "../components/Footer.js";
 import Navbar from "../components/Navbar.js";
 import Banner from "../components/Banner.js";
+import infocircle from "../pages/images/info-circle.png";
+import "./Main.css";
 
 export default function NewEntry() {
   const [allentrys, setAllEntrys] = useState([]);
-
 
   const options = {
     url: "https://sugarlybackend.herokuapp.com/entrys/add",
     method: "GET",
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
+      "Content-Type": "application/json;charset=UTF-8"
+    }
   };
 
   const getData = function () {
@@ -46,40 +49,46 @@ export default function NewEntry() {
     // console.log(mahlzeiten2);
 
     return mahlzeiten.map((e) => (
-      <div>
-        <table class="table table-hover">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Menge (in g)</th>
-              <th scope="col">Brennwert (Kcal)</th>
-              <th scope="col">Fett</th>
-              <th scope="col">Kohlenhydrate</th>
-              <th scope="col">davon Zucker</th>
-              <th scope="col">Protein</th>
-              <th scope="col">Ballaststoffe</th>
-              <th scope="col">BE</th>
-              <th scope="col">KE</th>
-              <th scope="col">FPE</th>
-            </tr>
-          </thead>
-          <tbody>
-            <th scope="row">{e.id}</th>
-            <td>{e.name}</td>
-            <td>{e.menge}</td>
-            <td>{e.brennwert}</td>
-            <td>{e.fett}</td>
-            <td>{e.kohlenhydrate}</td>
-            <td>{e.davonzucker}</td>
-            <td>{e.protein}</td>
-            <td>{e.ballaststoffe}</td>
-            <td>{e.kohlenhydrate / 12}</td>
-            <td>{e.kohlenhydrate / 10}</td>
-            <td>{(e.fett * 9 /100) + (e.protein * 4 /100) } </td>
-          </tbody>
-        </table>
-      </div>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            {e.name}
+            <img src={infocircle} alt="Info" width="25" height="25" />
+          </Accordion.Header>
+          <Accordion.Body>
+            <div>
+              <table class="table table-hover watisdrin">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">Menge (in g)</th>
+                    <th scope="col">Brennwert (Kcal)</th>
+                    <th scope="col">Fett</th>
+                    <th scope="col">Kohlenhydrate</th>
+                    <th scope="col">davon Zucker</th>
+                    <th scope="col">Protein</th>
+                    <th scope="col">Ballaststoffe</th>
+                    <th scope="col">BE</th>
+                    <th scope="col">KE</th>
+                    <th scope="col">FPE</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <th scope="row">{e.menge}</th>
+                  <td>{e.brennwert}</td>
+                  <td>{e.fett}</td>
+                  <td>{e.kohlenhydrate}</td>
+                  <td>{e.davonzucker}</td>
+                  <td>{e.protein}</td>
+                  <td>{e.ballaststoffe}</td>
+                  <td>{e.kohlenhydrate / 12}</td>
+                  <td>{e.kohlenhydrate / 10}</td>
+                  <td>{(e.fett * 9) / 100 + (e.protein * 4) / 100} </td>
+                </tbody>
+              </table>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     ));
   }
 
@@ -90,16 +99,16 @@ export default function NewEntry() {
     }, 0);
     if (zielwert === "BE") {
       return (
-        <p>
+        <p className="be-ergebnis">
           {" "}
-          Die gesamt {zielwert} betragen: {gesamt / 12}
+          <strong>Die gesamt {zielwert} betragen:</strong> {gesamt / 12}
         </p>
       );
     } else if (zielwert === "KE") {
       return (
-        <p>
+        <p className="ke-ergebnis">
           {" "}
-          Die gesamt {zielwert} betragen: {gesamt / 10}
+          <strong>Die gesamt {zielwert} betragen:</strong> {gesamt / 10}
         </p>
       );
     }
@@ -112,76 +121,66 @@ export default function NewEntry() {
     }, 0);
     let gesamt_protein = mahlzeiten3.reduce((total, item) => {
       return total + item.protein;
-    }, 0);console.log(gesamt_fett,gesamt_protein)
+    }, 0);
+    console.log(gesamt_fett, gesamt_protein);
     return (
-      <p>
-        Die gesamt FPE betragen{(gesamt_fett * 9/100) + (gesamt_protein * 4/100)}
+      <p className="fpe-ergebnis">
+        <strong>Die gesamt FPE betragen:</strong>{" "}
+        {(gesamt_fett * 9) / 100 + (gesamt_protein * 4) / 100}
       </p>
     );
-    
   };
 
   return (
     <div>
       <Navbar />
-      <Banner title="Neuer Eintrag" />
-      <div className="container">
-        <h2>Frühstück</h2>
-        <div>{findmahlzeit("Frühstück")}</div>
-        <div>
-          <p>{gesamt_berechnen("Frühstück", "BE")}</p>
-          <p>{gesamt_berechnen("Frühstück", "KE")}</p>
-          <p>{gesamt_berechnen_FPE("Frühstück")}</p>
+      <Banner title="Meine Einträge" />
+      <div className="container text-left main-content">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="mahlzeit">
+              <h2 className="h2-eintrag">Frühstück</h2>
+              <div>{findmahlzeit("Frühstück")}</div>
+              <div className="ergebnis-eintrag">
+                {gesamt_berechnen("Frühstück", "BE")}
+                {gesamt_berechnen("Frühstück", "KE")}
+                {gesamt_berechnen_FPE("Frühstück")}
+              </div>
+            </div>
+
+            <div className="mahlzeit">
+              <h2 className="h2-eintrag">Mittag</h2>
+              <div>{findmahlzeit("Mittagessen")}</div>
+              <div className="ergebnis-eintrag">
+                {gesamt_berechnen("Mittagessen", "BE")}
+                {gesamt_berechnen("Mittagessen", "KE")}
+                {gesamt_berechnen_FPE("Mittagessen")}
+              </div>
+            </div>
+
+            <div className="mahlzeit">
+              <h2 className="h2-eintrag">Abendessen</h2>
+              <div>{findmahlzeit("Abendbrot")}</div>
+              <div className="ergebnis-eintrag">
+                {gesamt_berechnen("Abendbrot", "BE")}
+                {gesamt_berechnen("Abendbrot", "KE")}
+                {gesamt_berechnen_FPE("Abendbrot")}
+              </div>
+            </div>
+
+            <div className="mahlzeit">
+              <h2 className="h2-eintrag">Spätmahlzeit</h2>
+              <div>{findmahlzeit("Nachts")}</div>
+              <div className="ergebnis-eintrag">
+                {gesamt_berechnen("Nachts", "BE")}
+                {gesamt_berechnen("Nachts", "KE")}
+                {gesamt_berechnen_FPE("Nachts")}
+              </div>
+            </div>
+          </div>
         </div>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
-
-        <h2>Zwischenmahlzeit 1</h2>
-        <p>
-          Maecenas dapibus, est posuere eleifend rutrum, lectus ligula gravida
-          urna, at pretium dui turpis non lorem.
-        </p>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
-
-        <h2>Mittag</h2>
-        <div>
-        <p>{findmahlzeit("Mittagessen")}</p>
-        <div><p>{gesamt_berechnen("Mittagessen", "BE")}</p>
-        <p>{gesamt_berechnen("Mittagessen", "KE")}</p>
-          <p>{gesamt_berechnen_FPE("Mittagessen")}</p>
-          </div>
-          </div>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
-        <h2>Zwischenmahlzeit 2</h2>
-        <p>
-          Maecenas dapibus, est posuere eleifend rutrum, lectus ligula gravida
-          urna, at pretium dui turpis non lorem.
-        </p>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
-        <h2>Abendessen</h2>
-        <div>
-        <p>{findmahlzeit("Abendbrot")}</p>
-        <div>
-        <p>{gesamt_berechnen("Abendbrot", "BE")}</p>
-        <p>{gesamt_berechnen("Abendbrot", "KE")}</p>
-          <p>{gesamt_berechnen_FPE("Abendbrot")}</p>
-          </div>
-          </div>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
-        <h2>Spätmahlzeit</h2>
-        <p>{findmahlzeit("Nachts")}</p>
-        <div>
-        <p>{gesamt_berechnen("Nachts", "BE")}</p>
-        <p>{gesamt_berechnen("Nachts", "KE")}</p>
-          <p>{gesamt_berechnen_FPE("Nachts")}</p>
-          </div>
-        <p> &nbsp;</p>
-        <p> &nbsp;</p>
       </div>
+      <Prefooter />
       <Footer />
     </div>
   );
