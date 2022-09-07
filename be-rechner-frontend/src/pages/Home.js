ba475718a60ce2c8840583240526ba8fff54ad21
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-09-07 08:50:53 +0200

    Neue Texte

--- be-rechner-frontend/src/pages/Home.js
+++ be-rechner-frontend/src/pages/Home.js
@@ -10,37 +10,35 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <Banner title="Welcome Sugies!" />
+      <Banner title="Der Insulin-Rechner für Diabetiker" />
       <div className="container text-left main-content">
         <div className="row">
           <div className="col-md-6 text-center">
             <img src={lolli} alt="Lolli" width="420" height="420" />
           </div>
           <div className="col-md-6">
-            <h2>Sugary - leicht und selbstbewusst unterwegs...</h2>
+            <h2>Wie viel Insulin gebe ich ab?</h2>
             <p>
-              Sugary ist von Menschen mit Diabetes für Menschen mit Diabetes. In
-              der Sugary App findest du all deine wichtigen Diabetes-Daten,
-              Integrationen und manuellen Einträgen, bequem an einem Ort.
+              Wie berücksichtige ich die Glykämische Last? <br />
+              Wie berechne ich Fett-Protein-Einheiten? <br /> Welche
+              Lebensmittel kann ich idealerweise miteinander kombinieren?
             </p>
+            <h2>Berechnet unkompliziert deine Mahlzeiten</h2>
             <p>
-              Die Sugary App steht dir in deinem Diabetes-Alltag jederzeit zur
-              Seite und hilft dir dabei, motiviert und selbstbewusst dein
-              Monster zu zähmen!
+              Bessere Blutzuckerwerte durch genaueres berechnen. Berechnet die
+              KE, BE, und FPE deiner Mahlzeiten anhand des Gewichts.
             </p>
-            <h2>Sugary - du brauchst sie einfach!</h2>
+
+            <h2>Speichere Mahlzeiten ab</h2>
             <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-              sanctus est Lorem ipsum dolor sit amet. At vero eos et accusam et
-              justo duo dolores et ea rebum.
+              Speichere Mahlzeiten und ihr Gewicht ab sodass du ständig darauf
+              zugreifen kannst.
             </p>
+
+            <h2>Füge eigene Lebensmittel hinzu</h2>
             <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua.{" "}
+              Füge Lebensmittel die nicht in der Datenbank vorhanden sind zu der
+              Liste bereits vorhandener Lebensmittel hinzu.
             </p>
           </div>
         </div>
