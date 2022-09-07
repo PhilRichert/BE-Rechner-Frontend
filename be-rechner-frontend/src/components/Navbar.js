4ce03d4ac8e9270f6984f3a4c4a70466e3abadf2
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-09-07 11:30:18 +0200

    Button Home - "active" weg

--- be-rechner-frontend/src/components/Navbar.js
+++ be-rechner-frontend/src/components/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto ">
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link className="nav-link" to="/">
                   Home <span className="sr-only">(current)</span>
                 </Link>
